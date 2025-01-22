653f9220fe39c45ad068f089b334f1aa2e764033
Precious <preciousbartholomew209@gmail.com>
2025-01-22 16:17:59 +0100

    added a new project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-[5vh] text-center '> Hello World </h2>
+    </div>
+  )
+  }
+
+  export default App;
