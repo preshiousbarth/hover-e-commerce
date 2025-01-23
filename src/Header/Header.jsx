d5a7d58ee0ede87e900db70ff16b60db26e1010f
Precious <preciousbartholomew209@gmail.com>
2025-01-23 16:51:56 +0100

    update

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import styles from './Header.module.scss'
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import styles from "./Header.module.scss";
+import { toast } from "react-toastify";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { FaShoppingCart, FaTimes, FaUserCircle } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+
 const Header = () => {
   return (
     <div>
